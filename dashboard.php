86ccee7cf01163663767377e7c127e61065d4fe6
Jay Chandel <97594783+Jaych36@users.noreply.github.com>
2022-02-18 13:26:05 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,34 @@
+
+<?php
+session_start();
+if(!isset($_SESSION['detailsdata']))
+{
+    header("location: ../");
+}
+
+?>
+
 <html>
     <head>
         <title>Online Voting System</title>
+        <link rel="stylesheeet" href="../css/style.css">
     </head>
     <body>
-        <button>Back</button>
-        <button>Logout</button>
+        <div id="headerSection">   
+
+        <button id="backbtn">Back</button>
+        <button id="logoutbtn" >Logout</button>
 
         <h1>Online Voting System</h1>
         <hr>
         <div id="Profile"></div>
-        <div id="Candidate"></div>
+        <div id="Candidate">
+
+        </div>
+
+
+
+        </div>
+       
     </body>
 </html>
