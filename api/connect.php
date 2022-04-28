ce7ee00702c0ebc9fe10167e5932b17432204dc2
Jay Chandel <97594783+Jaych36@users.noreply.github.com>
2022-04-28 16:11:12 +0530

    Delete connect.php

--- api/connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$serverName ="localhost:3307";
-$userName ="root";
-$password ="";
-$dbname ="vote";
-
-$connect = mysqli_connect($serverName, $userName,$password,$dbname) or die("connection fail");
-?>
